6e5e4c6fe0e9f19fdbce71afcfbf56175fa3b362
fransstrom <frans.herrstrom@gmail.com>
2025-10-30 10:31:22 +0100

    thi->

--- Position.cpp
+++ Position.cpp
@@ -1,15 +1,12 @@
 #include "Position.hpp"
 #include <iostream>
 
-void Position::move(double dx, double dy) 
-{
-    x += dx;
-    y += dy;
-
-    return;
+void Position::move(double dx, double dy) {
+  this->x += dx;
+  this->y += dy;
+  return;
 };
 
-void Position::print() const 
-{
-    std::cout << "\nPosition X:\t" << x << "\nPosition Y:\t" << y << "\n";
+void Position::print() const {
+  std::cout << "\nPosition X:\t" << x << "\nPosition Y:\t" << y << "\n";
 }
